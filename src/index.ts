40182fc31aced7811306a59c4ec5c4bbf3fd5b2a
jh2oman <jacobwaterman@MacBook-Pro.local>
2022-03-14 14:09:53 -0700

    fix(quote): amounts should be type string

--- src/index.ts
+++ src/index.ts
@@ -44,10 +44,10 @@ export type QuoteResponse = {
 
 export type QuoteErrorResponse = {
   error: FiatConnectError
-  minimumFiatAmount?: number
-  maximumFiatAmount?: number
-  minimumCryptoAmount?: number
-  maximumCryptoAmount?: number
+  minimumFiatAmount?: string
+  maximumFiatAmount?: string
+  minimumCryptoAmount?: string
+  maximumCryptoAmount?: string
 }
 
 // Helper type
