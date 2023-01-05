c7c0e6db5d7f83564d6a1ed494c6207518e95533
Ailson da Cruz <ailsoncgt@gmail.com>
2023-01-05 11:34:18 -0300

    fix(accounts): fix PIX random key regex

--- src/fiat-account.ts
+++ src/fiat-account.ts
@@ -62,7 +62,7 @@ export const PIX_EMAIL_KEY_REGEX =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ // credit to http://emailregex.com/
 export const PIX_CPF_KEY_REGEX = /^([0-9]{3}\.){2}[0-9]{3}[-]([0-9]{2})$/ // example: 000.000.000-00, see https://en.wikipedia.org/wiki/CPF_number
 export const PIX_PHONE_KEY_REGEX = /^[0-9]{11}$/
-export const PIX_RANDOM_KEY_REGEX = /^[a-zA-Z0-9]{32}$/
+export const PIX_RANDOM_KEY_REGEX = /^[a-zA-Z0-9-]{32}$/
 
 export const pixAccountSchema = requiredFiatAccountSchemaFieldsSchema
   .and(
